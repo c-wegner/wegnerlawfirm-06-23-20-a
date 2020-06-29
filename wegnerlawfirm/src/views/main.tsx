eba275def5d1f451e1edca2dfe1df0c349ab16c6
cwegner <cwegner@wegnerlawfirm.com>
2020-06-29 13:27:39 +0000

    Scrolling working

--- wegnerlawfirm/src/views/main.tsx
+++ wegnerlawfirm/src/views/main.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { scroll } from '../components/tools/scroll';
 
-const Main=()=>(
-    <div>
-        Place stuff here src/views/main.tsx
-    </div>
-)
+
+const Main = () => {
+    const refHero= useRef(null);
+
+    function handleNavigation(){
+        scroll(refHero.current.offsetTop);
+    }
+
+    return (
+        <div>
+            Place stuff here src/views/main.tsx
+            <div style={{ height: '120vh' }} ref={refHero}></div>
+            <div style={{ height: '120vh' }} ></div>
+            <div style={{ height: '120vh' }}>
+                <button onClick={handleNavigation}>ddd</button>
+            </div>
+        </div>
+    )
+}
 
 export default Main;
